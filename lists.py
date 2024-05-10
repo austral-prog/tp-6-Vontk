9116b3aaacca2ad65bc3ad69477a776d88d21a2e
Matias P. Menendez <121203087+Vontk@users.noreply.github.com>
2024-05-10 12:56:11 -0300

    Update lists.py

--- lists.py
+++ lists.py
@@ -12,10 +12,10 @@ def is_empty(list_to_check):
 
 def check_lists(list_to_compare1, list_to_compare2):
     if len(list_to_compare1) > 3 and len(list_to_compare2) > 3:
-        return list_to_compare1[3] == list_to_compare2[3]
+        return list_to_compare1[2] == list_to_compare2[2]
     else:
         return False
 
 
 def list_of_lists(list_of_lists_to_modify):
-    return [[list_of_lists_to_modify[0][:2]], [list_of_lists_to_modify[1][1:4]], [list_of_lists_to_modify[2][-2:]]]
+    return [list_of_lists_to_modify[0][:2], list_of_lists_to_modify[1][1:4], list_of_lists_to_modify[2][-2:]]
